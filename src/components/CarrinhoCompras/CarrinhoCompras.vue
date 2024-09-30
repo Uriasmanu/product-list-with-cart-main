a218ee663ddbebd7bfe4123f6489d655d8808994
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-09-30 14:57:05 -0300

    criação dos itens do carrinho

--- src/components/CarrinhoCompras/CarrinhoCompras.vue
+++ src/components/CarrinhoCompras/CarrinhoCompras.vue
@@ -6,13 +6,28 @@
             <p>Seus itens adicionados aparecerão aqui</p>
         </div>
     </div>
+
+    <div class="container-carrinho-itens">
+        <h3>Seu Carrinho({{ quantidadeItens }})</h3>
+        <div class="infos">
+            <ItensCarrinho />
+        </div>
+    </div>
 </template>
 
 <script>
+import ItensCarrinho from '../ItensCarrinho/ItensCarrinho.vue';
+
 export default {
     name: 'CarrinhoCompras',
     components: {
+        ItensCarrinho
+    },
 
+    data(){
+        return {
+            quantidadeItens: 0
+        }
     }
 }
 </script>
---
+++ src/components/ItensCarrinho/ItensCarrinho.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-itens">
+        <div class="row">
+            <p class="nome">classic tiramissu</p>
+            <div class="valor">
+                <p>2x</p>
+                <p>@ R$ 5,50</p>
+                <p>R$ 11,00</p>
+            </div>
+        </div>
+        <img src="../../assets/images/icon-remove-item.svg" alt="icone remover">
+    </div>
+</template>
+
+<script>
+export default {
+    nome: "ItensCarrinho"
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import './_ItensCarrinho';
+</style>
