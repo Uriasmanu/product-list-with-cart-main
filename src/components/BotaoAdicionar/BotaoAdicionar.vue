485003d87b9a3d48b56ee8b3c98abb54fe954a05
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-09-29 21:41:13 -0300

    configuração da reação do botao ao clicar

--- src/components/BotaoAdicionar/BotaoAdicionar.vue
+++ src/components/BotaoAdicionar/BotaoAdicionar.vue
@@ -1,22 +1,51 @@
 <template>
-    <div class="container-botao-adicionar">
-        <img src="../../assets/images/icon-add-to-cart.svg" alt="">
+    <div v-if="!ativo" class="container-botao-adicionar" @click="ativarBotao">
+        <img src="../../assets/images/icon-add-to-cart.svg" alt="icone de carrinho">
         <p>Adicionar</p>
     </div>
-    <div class="container-botao-adicionar-ativo">
-        <img src="../../assets/images/icon-decrement-quantity.svg" alt="">
-        <p>1</p>
-        <img src="../../assets/images/icon-increment-quantity.svg" alt="">
+    <div v-else class="container-botao-adicionar-ativo">
+        <button @click="remover"><img src="../../assets/images/icon-decrement-quantity.svg"
+                alt="icone remover"></button>
+        <p>{{ valor }}</p>
+        <button @click="adicionar"><img src="../../assets/images/icon-increment-quantity.svg"
+                alt="icone adicionar"></button>
+
     </div>
 </template>
 
 <script>
-    export default{
-        nome: 'BotaoAdicionar',
-        components: {
-
+export default {
+    nome: 'BotaoAdicionar',
+    data() {
+        return {
+            valor: 1,
+            ativo: false
         }
+    },
+
+    methods: {
+        ativarBotao() {
+            this.ativo = true
+        },
+
+        adicionar() {
+            this.valor++;
+        },
+
+        remover() {
+            if (this.valor > 1) {
+                this.valor--;
+
+            } else {
+                this.ativo = false;
+            }
+        }
+
     }
+}
+
+
+
 </script>
 
 <style lang="scss" scoped>
